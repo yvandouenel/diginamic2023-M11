7d9af581710a23a930c0181fb3f2c4d34618ab13
yvandouenel <y.douenel@coopernet.fr>
2024-02-09 10:23:45 +0100

    Ajout du composant velomobile.

---
+++ react/velomobile/src/components/VeloMobile.jsx
@@ -0,0 +1,10 @@
+const VeloMobile = (props) => {
+  return (
+    <>
+      <h3>{props.veloMobile.model}</h3>
+      <p>{props.veloMobile.description}</p>
+    </>
+  );
+}
+
+export default VeloMobile;
--- react/velomobile/src/routes/ProductsPage.jsx
+++ react/velomobile/src/routes/ProductsPage.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
+import VeloMobile from './../components/VeloMobile';
+/**
+ * Composant fonction
+ * @returns JSX
+ */
 const ProductsPage = () => {
+  // Création d'un état (velosMobiles), création d'un setter correspondant
+  // Assignation d'une valeur par défaut
+  const [velosMobiles, setVelosMobiles] = useState([
+    {
+      id: 1,
+      model: "lvf",
+      description: "Le vélo mobile français qui a fait ses preuves",
+      photo: "lvf.jpg",
+      weight: 32
+    },
+    {
+      id: 2,
+      model: "lvq",
+      description: "Le vélo mobile sportif aux performances incroyables",
+      photo: "lvq.jpg",
+      weight: 22
+    }
+  ]);
   return (
-    <h2>Produits</h2>
+    <>
+      <h2>Produits</h2>
+      {/* Affichage de la listes des vélos mobiles */}
+      {velosMobiles.map((veloMobile) => <VeloMobile veloMobile={veloMobile} />)}
+    </>
   );
 }
 
