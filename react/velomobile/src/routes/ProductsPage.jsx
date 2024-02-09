d52ea7f65474c3cb0c1a88c6dd7b1a8c66feb0b2
yvandouenel <y.douenel@coopernet.fr>
2024-02-09 14:34:56 +0100

    Gestion de l'erreur non pas dans le service mais dans le composant qui l'appelle.

--- react/velomobile/src/routes/ProductsPage.jsx
+++ react/velomobile/src/routes/ProductsPage.jsx
@@ -9,6 +9,8 @@ const ProductsPage = () => {
   // Création d'un état (velosMobiles), création d'un setter correspondant
   // Assignation d'une valeur par défaut
   const [velosMobiles, setVelosMobiles] = useState([]);
+  const [errorMsg, setErrorMsg] = useState("");
+
 
   // Programmation asynchrone : le code suivant ne va s'exécuter qu'après le premier chargement (render) du composant (ici ProductsPage)
   useEffect(() => {
@@ -19,14 +21,19 @@ const ProductsPage = () => {
         // Modification du state qui va impliquer un rechargement de la vue
         // c'est à dire le rappel de render
         setVelosMobiles(remoteVelosMobiles);
+      })
+      .catch(error => {
+        console.log(`Erreur attrapée dans useEffect : `, error);
+        setErrorMsg(error.toString());
       });
   }, [])
   console.log(`dans ProductsPage`);
   return (
     <>
       <h2>Produits</h2>
-      {/* Affichage de la listes des vélos mobiles */}
-      {velosMobiles.map((veloMobile) => <VeloMobile veloMobile={veloMobile} />)}
+      {errorMsg}
+      {/* Affichage de la listes des vélos mobiles sous condition que velosMobiles est "truely" */}
+      {velosMobiles && velosMobiles.map((veloMobile) => <VeloMobile veloMobile={veloMobile} />)}
     </>
   );
 }
--- react/velomobile/src/services/RemoteData.js
+++ react/velomobile/src/services/RemoteData.js
@@ -1,7 +1,7 @@
 export default class RemoteData {
-  static url = "http://localhost:3001/velosMobiles";
+  static url = "http://localhost:3001/velosMobilesss";
   /**
-   * L'effet global de cette méthode est d'envoyer une requête à un serveur distant, de vérifier si la requête a réussi, d'analyser la réponse au format JSON, de consigner la réponse et de renvoyer les données analysées. Si une étape échoue, il enregistre un message d'erreur.
+   * L'effet global de cette méthode est d'envoyer une requête à un serveur distant, de vérifier si la requête a réussi, d'analyser la réponse au format JSON, de l'afficher dans la console  et de la renvoyer. Si une étape échoue, il affiche un message d'erreur.
    * 
    * @returns Promise<{}[]>
    */
@@ -11,14 +11,12 @@ export default class RemoteData {
         console.log(`response.status`, response.status);
         if (response.status == 200) {
           return response.json();
-        } else throw new Error("Problème de serveur dans loadVelosMobiles")
+        } else throw new Error("Problème de serveur dans loadVelosMobiles. Statut de l'erreur : " + response.status)
       })
       .then((velosMobiles) => {
         console.log(`velosMobiles`, velosMobiles);
         return velosMobiles;
       })
-      .catch(error => {
-        console.log(`Erreur attrapée : ` + error);
-      })
+
   }
 }
